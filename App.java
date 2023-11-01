8a4d5596113b797428fe8540c21da62570c02c59
leogloria <leonardo.gloria@prof.infnet.edu.br>
2023-10-31 21:29:50 -0300

    Resolvendo Bug na classe main

--- App.java
+++ App.java
@@ -1,6 +1,6 @@
 public class App {
     public static void main(String ... args){
         System.out.println("Rodando o Metodo Main");
-        System.out.println("Meio do Método Main");
+        System.out.println("Meio do método Main");
     }
 }
