7cd7e13a56ba7a37733c678c9d80cd21889b41cd
leogloria <leonardo.gloria@prof.infnet.edu.br>
2023-10-31 21:40:37 -0300

    Finalizando o metodo main

--- App.java
+++ App.java
@@ -2,5 +2,6 @@ public class App {
     public static void main(String ... args){
         System.out.println("Rodando o Metodo Main");
         System.out.println("Meio do método Main");
+        System.out.println("Final do método Main");
     }
 }
