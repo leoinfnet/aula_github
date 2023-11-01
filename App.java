37739c1b4abe8652d56848123961d48347e139e6
leonardogloria <contato@leonardogloria.com.br>
2023-10-31 21:46:29 -0300

    Corrigindo Bug esquisito

--- App.java
+++ App.java
@@ -1,7 +1,7 @@
 public class App {
     public static void main(String ... args){
         System.out.println("Rodando o Metodo Main");
-        System.out.println("Meio do método Main");
-        System.out.println("Final do método Main");
+        System.out.println("Meio do metodo Main");
+        System.out.println("Final do metodo Main");
     }
 }
