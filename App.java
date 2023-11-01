a035800ca20891b759025a150047e97a78b8a043
= <jp@teste.com>
2023-10-31 21:50:11 -0300

    ACertando nomeclatura

--- App.java
+++ App.java
@@ -1,6 +1,6 @@
 public class App {
     public static void main(String ... args){
-        System.out.println("Rodando o Metodo Main");
+        System.out.println("Iniciando o Metodo Main");
         System.out.println("Meio do metodo Main");
         System.out.println("Final do metodo Main");
     }
