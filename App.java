64e44353e9370271a3ef0211fb07a0ddb0f3084c
leogloria <leonardo.gloria@prof.infnet.edu.br>
2023-11-07 20:50:45 -0300

    Corrigido bug no titulo

--- App.java
+++ App.java
@@ -1,6 +1,6 @@
 public class App {
     public static void main(String ... args){
-        System.out.println("Iniciando o Metodo Main");
+        System.out.println("Iniciando o Metodo main");
         System.out.println("Meio do metodo Main");
         System.out.println("Final do metodo Main");
     }
