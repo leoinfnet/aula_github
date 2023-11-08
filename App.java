2f8e2bd9ca9c6af98ce30a7bfe90a6e401345dfd
= <jp@teste.com>
2023-11-07 21:34:01 -0300

    Revert "nova media"
    This reverts commit 56eadf39f0086bf64c83524bb7caf1b9f04e81fe.

--- App.java
+++ App.java
@@ -10,7 +10,4 @@ public class App {
     public int mediaPonderada(int valores){
         System.out.println("Calculando Media Ponderada");
     }
-    public int mediaGeometroca(int valores){
-        System.out.println("Calculando Media Ponderada");
-    }
 }
